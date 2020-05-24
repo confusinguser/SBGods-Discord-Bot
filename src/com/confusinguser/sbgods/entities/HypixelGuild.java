f77ab88609f2da9cc0c0158c3bbe7d938833d70d
Soopyboo32 <Soopyboo32@gmail.com>
2020-05-24 14:39:40 +0800

    k

--- src/com/confusinguser/sbgods/entities/HypixelGuild.java
+++ src/com/confusinguser/sbgods/entities/HypixelGuild.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public enum HypixelGuild {
     SBG("5cd01bdf77ce84cf1204cd61", 300000, 26, "Skyblock Gods", "SBG Guild Member", "SBG"),
-    SBDG("5e4e6d0d8ea8c9feb3f0e44f", 1000, 1, "Skyblock Forceful", "SBF", "SBF Guild Member", "SBDG Guild Member", "Skyblock Demigods", "SBDG", "Skyblock Demi Gods");
+    SBDG("5e4e6d0d8ea8c9feb3f0e44f", 1000, 10, "Skyblock Forceful", "SBF", "SBF Guild Member", "SBDG Guild Member", "Skyblock Demigods", "SBDG", "Skyblock Demi Gods");
                                                     //Error was tryint to use SBGods.getInstance() before creation
     private final String guildId;
     private final int skillReq;
