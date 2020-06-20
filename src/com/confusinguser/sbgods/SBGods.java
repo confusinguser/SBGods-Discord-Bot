dcbbafa917930916a5689fd7987b31e591d3d140
ConfusingUser <25439218+confusinguser@users.noreply.github.com>
2020-06-20 18:48:40 +0200

    Version 0.9.3.2

--- src/com/confusinguser/sbgods/SBGods.java
+++ src/com/confusinguser/sbgods/SBGods.java
@@ -11,15 +11,15 @@ import java.util.Date;
 import java.util.logging.*;
 
 public class SBGods {
-    public static final String VERSION = "0.9.3.1";
+    public static final String VERSION = "0.9.3.2";
     public static final String VERSION_DESCRIPTION_MAJOR = ""; // Change this every major release: 0.9.6.3 -> 1.0
     public static final String VERSION_DESCRIPTION_MINOR = "Added the verify list"; // Change this every minor release: 0.8.11.5 -> 0.8.12
-    public static final String VERSION_DESCRIPTION_PATCH = "Fixed bugs."; // Change this every patch: 0.8.11.4 -> 0.8.11.5
+    public static final String VERSION_DESCRIPTION_PATCH = "Fixed more bugs"; // Change this every patch: 0.8.11.4 -> 0.8.11.5
     public static final String[] DEVELOPERS = {"244786205873405952", "497210228274757632"};
     private static final DiscordServer[] servers = {DiscordServer.SBGods, DiscordServer.SBDGods}; // For release on main servers
     //private static final DiscordServer[] servers = {DiscordServer.Test}; // For testing
     private static SBGods instance;
-    public final String[] keys = {"bc90572a-1547-41a5-8f28-d7664916a28d", "3963906e-ffb6-45b9-b07b-80ca9838eb20", "a269efda-d93f-4521-a7a1-b793166d9ca3", "7673c96b-95b0-429d-81b6-031d9e249c75", "fbb3bea4-0f61-4b5f-a656-502403a3e7c5", "13d28584-99db-4473-a068-467498fcaa8b"};//Soopy's keys
+    public final String[] keys = {"bc90572a-1547-41a5-8f28-d7664916a28d", "3963906e-ffb6-45b9-b07b-80ca9838eb20", "a269efda-d93f-4521-a7a1-b793166d9ca3", "7673c96b-95b0-429d-81b6-031d9e249c75", "fbb3bea4-0f61-4b5f-a656-502403a3e7c5", "13d28584-99db-4473-a068-467498fcaa8b"};//Soopy's keys, dam thats a lot of keys
     public final Logger logger = Logger.getLogger(this.getClass().getName());
     private final ApiUtil apiutil;
     private final Util util;
